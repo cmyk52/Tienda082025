31e21e3083cff655455487b0bd79e6d4a3833106
JosmarCL <j.lmarambio89@gmail.com>
2025-08-18 21:48:20 -0400

    pagos

---
+++ src/script/pago.js
@@ -0,0 +1,67 @@
+const submit = document.getElementById('submit')
+
+
+// Capturar eveto clic a Submit 
+
+submit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    valores()
+})
+
+
+// Capturar valores del formulario
+
+function valores(){
+
+    const listaValores = {
+        nombre: document.getElementById('nombre').value,
+        apellido: document.getElementById('apellido').value,
+        direccion: document.getElementById('direccion').value,
+        numeral: document.getElementById('numeral').value,
+        telefono: document.getElementById('telefono').value,
+        correo: document.getElementById('correo').value,
+        entrega: document.getElementById('entrega').value
+
+    }
+
+    
+    validacionDatos(listaValores)
+}
+
+// Validacion de datos
+
+function validacionDatos(datosEvaluados){
+
+
+    const msjAlert = document.getElementById('error')
+    let html = ''
+    console.log(msjAlert)
+
+
+    if( datosEvaluados.nombre === '' || 
+        datosEvaluados.apellido === '' || 
+        datosEvaluados.direccion === '' || 
+        datosEvaluados.numeral === '' || 
+        datosEvaluados.telefono === '' ||
+        datosEvaluados.correo === '' ||
+        datosEvaluados.entrega === '' 
+    )
+        
+        {
+
+        console.log('faltan datos')
+        html += 'Faltan campos por completar'
+        msjAlert.innerHTML = html
+
+    } 
+    
+    else 
+        {
+        console.log('valores ok')
+        html += ''
+        msjAlert.innerHTML = html
+    }
+
+
+}
+
